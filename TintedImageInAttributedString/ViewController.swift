3689a047525c44c24f3a1719e359edcbd9665906
Jean-Dominique Nguele <jd@lodgeo.com>
2016-04-29 08:20:45 +0100

    Sources added

---
+++ TintedImageInAttributedString/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TintedImageInAttributedString
+//
+//  Created by JD on 28/04/2016.
+//  Copyright © 2016 IamNguele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelWithColoredImage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+

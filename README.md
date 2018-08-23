96375a2748a1c44cd24403ec1d6e13235f8a4698
Jean-Dominique Nguele <contact@iamnguele.com>
2018-08-23 23:04:17 +0100

    Update with XCode 10.0 beta 6 + iOS 12 + Swift 4

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TintedImageInAttributedString
 
-This is the sources for the tutorial about setting a color for a loaded image on a NSAttributedString. The tutorial is available at http://blog.iamnguele.com/2016/04/29/set-a-tint-color-on-an-image-in-a-nsattributedstring/
+This is the sources for the tutorial about setting a color for a loaded image on a NSAttributedString. The tutorial is available at https://www.codingnagger.com/2016/04/29/set-a-tint-color-on-an-image-in-a-nsattributedstring/
--- TintedImageInAttributedString/ViewController.swift
+++ TintedImageInAttributedString/ViewController.swift
@@ -2,61 +2,42 @@
 //  ViewController.swift
 //  TintedImageInAttributedString
 //
-//  Created by JD on 28/04/2016.
-//  Copyright © 2016 IamNguele. All rights reserved.
+//  Created by Jean-Dominique Nguele on 23/08/2018.
+//  Copyright © 2018 CodingNagger. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var labelWithColoredImage: UILabel!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        labelWithColoredImage.textColor = .blue
         
-        // set the label text color as red
-        labelWithColoredImage.textColor = .blueColor()
+        labelWithColoredImage.textAlignment = .center
         
-        // align the text to the center
-        labelWithColoredImage.textAlignment = .Center;
-        
-        // load the image
-        let image: UIImage = UIImage(named: "swift_logo")!.imageWithRenderingMode(.AlwaysTemplate)
-        
-        // create the text attachment
-        let textAttachment: NSTextAttachment = NSTextAttachment()
-        
-        // assign the loaded image to the text attachment
-        textAttachment.image = image
-        
-        // create an attributed string with our text attachment
-        let attributedStringWithImage: NSAttributedString = NSAttributedString(attachment: textAttachment);
-        
-        // now create the full string to display
-        let stringToDisplay = "That text written at 4:20AM\nfor reasons."
-        let fullAttributedString: NSMutableAttributedString =
-            NSMutableAttributedString(string: "\(stringToDisplay) ")
-        
-        // append space the string to display with our string containing the image
-        fullAttributedString.appendAttributedString(attributedStringWithImage)
-        
-        // color the image part of the string
-        fullAttributedString.addAttribute(
-            NSForegroundColorAttributeName,
-            value: UIColor.greenColor(),
-            range: NSMakeRange(
-                stringToDisplay.startIndex.distanceTo(
-                    stringToDisplay.endIndex), attributedStringWithImage.length))
-        
-        // assign it to the label
-        labelWithColoredImage.attributedText = fullAttributedString;
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if let image = UIImage(named:  "swift_logo", in: Bundle.main, compatibleWith: nil) {
+            let templateImage = image.withRenderingMode(.alwaysTemplate)
+            
+            let textAttachment = NSTextAttachment()
+            textAttachment.image = templateImage
+            
+            let attributedStringWithImage = NSAttributedString(attachment: textAttachment);
+            
+            let stringToDisplay = "That text written at 4:20AM\nfor reasons."
+            
+            let fullAttributedString = NSMutableAttributedString(string:"\(stringToDisplay) ")
+            
+            fullAttributedString.append(attributedStringWithImage)
+            
+            fullAttributedString.addAttribute(
+                NSAttributedString.Key.foregroundColor,
+                value: UIColor.green,
+                range: NSMakeRange(
+                    stringToDisplay.count, attributedStringWithImage.length))
+            
+            labelWithColoredImage.attributedText = fullAttributedString
+        }
     }
 }
 
